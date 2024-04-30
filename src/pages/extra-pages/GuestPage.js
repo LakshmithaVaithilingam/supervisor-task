700d6f862bc2bcd4f88baec82c8567c1479b8313
Lakshmitha Vaithilingam <lakshmitha.20210713@iit.ac.lk>
2024-04-30 14:23:04 +0530

    signOut GuestPage.js

--- src/pages/extra-pages/GuestPage.js
+++ src/pages/extra-pages/GuestPage.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Button } from '@mui/material';
+import { useDispatch } from 'react-redux';
+import { signOut } from 'firebase/auth'; 
+import { auth } from '  FirebaseConfig'; 
+import { googleLogout } from 'store/actions/OauthActions';
+import { useNavigate } from 'react-router-dom';
 
 const GuestPage = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+      navigate("/login");
+      dispatch(googleLogout());
+    } catch (error) {
+      console.error('Error signing out:', error);
+    }
+  };
+
   return (
     <div>
       <h1>Hi, Guest!</h1>
       <p>Welcome to our website. Please sign in to access more features.</p>
+      <Button variant="outlined" color="secondary" onClick={handleSignOut}>
+        Sign Out
+      </Button>
     </div>
   );
 };
